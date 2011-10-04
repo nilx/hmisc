e9f9e366a7a9ca7ec58e0e13bea80491f9ea0860
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2011-10-04 23:54:38 +0200

    beautify

--- debug.h
+++ debug.h
@@ -256,7 +256,7 @@ static _LL _dbg_cpucycles(void)
 {
     unsigned _LL result;
     __asm__ volatile (".byte 15;.byte 49;shlq $32,%%rdx;orq %%rdx,%%rax":"=a"
-                  (result)::"%rdx");
+                      (result)::"%rdx");
     return result;
 }
 
